27c78d58ba8d2ae7092c9bfdbe8a4c8c3eb0232a
Akash Deep Singh <82092014+akashdeep167@users.noreply.github.com>
2025-08-28 02:00:32 +0530

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,7 @@
 import app from "./app";
 
 const PORT = process.env.PORT || 3001;
-const HOST = '0.0.0.0';
 
-app.listen(PORT, HOST, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on PORT : ${PORT}`);
 });
