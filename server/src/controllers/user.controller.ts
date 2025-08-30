dfc3c0ba60be13882b034793eddcc138457e757e
Akash Deep Singh <82092014+akashdeep167@users.noreply.github.com>
2025-08-30 22:17:41 +0530

    Update user.controller.ts

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -56,17 +56,18 @@ export const login = async (req: Request, res: Response) => {
 
     res.cookie("accessToken", accessToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      secure: process.env.NODE_ENV === "production", // Railway = true, Local = false
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", 
       maxAge: 15 * 60 * 1000,
     });
 
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
+
 
     return res.json({
       message: "Login successful",
